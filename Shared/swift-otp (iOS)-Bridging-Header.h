15b0a07ce1b8910902d87358d501d4437b9ec4e1
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2021-01-29 15:55:37 +0800

    :fire: remove old c and swift wrapper

--- Shared/swift-otp (iOS)-Bridging-Header.h
+++ Shared/swift-otp (iOS)-Bridging-Header.h
@@ -3,19 +3,3 @@
 //
 
 #import "OcMiniAppInfo.h"
-
-const void * MiniAppInfo_ParseFile(const char * path);
-const char * MiniAppInfo_GetAppPath(const void * miniAppInfo);
-void MiniAppInfo_SetAppPath(const void * miniAppInfo, const char * path);
-const void * MiniAppInfo_GetRouterInfo(const void * miniAppInfo);
-const char * RouterInfo_GetEntry(const void * routerInfo);
-void RouterInfo_SetEntry(const void * routerInfo, const char * entry);
-struct mapEntry {
-    const char * key;
-    const char * value;
-};
-struct cppMap {
-    unsigned long size;
-    struct mapEntry * mapEntries;
-};
-const struct cppMap * MiniAppInfo_GetSubPackages(const void * miniAppInfo);
